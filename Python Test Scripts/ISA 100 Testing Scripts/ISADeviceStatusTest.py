fa9fdec304a7cde5b23e2ecf6ab9d58b909aa6fb
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-07-24 12:46:55 -0500

    Added Optional File Writing to ISADeviceStatusTest.py

--- Python Test Scripts/ISA 100 Testing Scripts/ISADeviceStatusTest.py
+++ Python Test Scripts/ISA 100 Testing Scripts/ISADeviceStatusTest.py
@@ -1,6 +1,8 @@
 # This is an example of how to use ISADeviceCount.py's IsaDeviceCounter class to download
 # an ISA database file from a gateway and read the device statuses
 
+WRITE_RESULTS = False
+
 from ISADeviceCount.ISADeviceCount import IsaDeviceCounter
 
 # Create the SSH and SCP connections
@@ -14,11 +16,19 @@ devices = gateway.get_isa_devices("Monitor_Host.db3")
 for entry in devices:
     print(str(entry) + ": " + str(devices[entry]))
 
+if WRITE_RESULTS:
+    with open("test.csv", "w") as f:
+        f.write("DeviceID,Reliability\n")
+
 # Calculate the reliability of each device and print to the console
 reliabilities = gateway.get_device_reliability("Monitor_Host.db3")
 for device in reliabilities:
     if reliabilities[device] != 100:
         print(str(device) + ": " + str(reliabilities[device]) + "%")
+
+    if WRITE_RESULTS:
+        with open("test.csv", "a") as f:
+            f.write(str(device) + "," + str(reliabilities[device]) + "\n")
 
 # Close the connections
 gateway.close()
