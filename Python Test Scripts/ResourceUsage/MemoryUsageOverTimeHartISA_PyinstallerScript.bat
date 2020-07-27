02309d55a1e1f334ff44cc3eb02c13e52b4dd70e
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-07-27 09:41:09 -0500

    Created requirements.txt to Simplify Build Process

--- Python Test Scripts/ResourceUsage/MemoryUsageOverTimeHartISA_PyinstallerScript.bat
+++ Python Test Scripts/ResourceUsage/MemoryUsageOverTimeHartISA_PyinstallerScript.bat
@@ -1,1 +1,3 @@
+pip.exe install pyinstaller
+pip.exe install -r requirements.txt
 pyinstaller.exe MemoryUsageOverTimeHartISA.py --clean --onefile --path "./../ISA 100 Testing Scripts/ISADeviceCount" --path "./../Gateway Web Scraping Tools/GwDeviceCount"
