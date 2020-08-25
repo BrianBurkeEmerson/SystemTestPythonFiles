b0c0cd299432d8f3f4d91fe79341124233b776eb
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-08-25 11:44:07 -0500

    Modified Original Program to Prepare for GUI Integration

--- Python Test Scripts/ResourceUsage/MemoryUsageOverTimeHartISA.py
+++ Python Test Scripts/ResourceUsage/MemoryUsageOverTimeHartISA.py
@@ -343,140 +343,6 @@ def record_data(filename, gateway, scraper, measurement_interval, track_hart, tr
 
 def main():
     global folder
-
-    # Create a dictionary for the various options
-    options = {}
-
-    # Create a parser that can handle INI files
-    config = ConfigParser()
-
-    # If no config file exists, create a new default one
-    if not(os.path.isfile(CONFIG_FILE_NAME)):
-        config["General"] = {
-            "UseSettingsFromConfigFile" : "no" # Whether the settings stored in the config file should be used
-        }
-        config["Gateway"] = {
-            "Hostname" : "192.168.1.10",
-            "Username" : "root",
-            "Password" : "emerson1"
-        }
-        config["WebBrowser"] = {
-            "WebUsername" : "admin",
-            "WebPassword" : "default"
-        }
-        config["DataRecording"] = {
-            "UseTimeLimit" : "no", # If set to no, user manually stops test
-            "TimeLimit" : "600", # Time limit for test in seconds
-            "MeasurementInterval" : "60", # How long between measurements
-            "TrackHART" : "yes", # If set to yes, the program records the number of connected HART devices (adds extra time)
-            "TrackISA" : "yes", # If set to yes, the program records the number of connected ISA devices
-            "ProcessesToTrack" : "" # The list of processes whose memory usage should be tracked (separated by a comma with on spaces)
-        }
-        config["Files"] = {
-            "UseAutomaticFilename" : "no" # If set to yes, the filename is automatically generated based on when the test started
-        }
-
-        # Write the config file
-        with open(CONFIG_FILE_NAME, "w") as config_file:
-            config.write(config_file)
-    
-    # Read the options from the config file
-    config.read(CONFIG_FILE_NAME)
-    for section in config.sections():
-        for item in config.items(section):
-            options[item[0]] = item[1]
-    
-    # Create the variables used for configuring the test
-    hostname = ""
-    username = ""
-    password = ""
-    use_time_limit = False
-    time_limit = 0
-    measurement_interval = 0
-    filename = ""
-    web_username = ""
-    web_password = ""
-    track_hart = True
-    track_isa = True
-    processes_to_track = []
-
-    # Check whether to used the saved settings
-    if options["UseSettingsFromConfigFile".lower()] == "yes":
-        hostname = options["Hostname".lower()]
-        username = options["Username".lower()]
-        password = options["Password".lower()]
-        use_time_limit = options["UseTimeLimit".lower()] == "yes"
-        time_limit = int(options["TimeLimit".lower()])
-        measurement_interval = int(options["MeasurementInterval".lower()])
-        web_username = options["WebUsername".lower()]
-        web_password = options["WebPassword".lower()]
-        track_hart = options["TrackHART".lower()] != "no"
-        track_isa = options["TrackISA".lower()] != "no"
-
-        # Parse the list of processes that should be tracked
-        processes_to_track = options["ProcessesToTrack".lower()].split(',')
-    else:
-        # Ask if the user wants to track HART, ISA, or both
-        print("1. Track HART devices only")
-        print("2. Track ISA devices only")
-        print("3. Track HART and ISA devices")
-        tracking = verify_int_input_option((1, 2, 3), "Select 1, 2, or 3: ")
-
-        # Convert tracking option to set booleans (note that they are both already set to True)
-        if tracking == 1:
-            track_isa = False
-        elif tracking == 2:
-            track_hart = False
-        
-        # Ask for the gateway SSH hostname, username, and password
-        hostname = input("Enter the gateway hostname: ")
-        # if track_isa:
-        #     username = input("Enter the SSH username (leave blank to use default): ")
-        #     password = getpass("Enter the SSH password (no echo) (leave blank to use default): ")
-        username = input("Enter the SSH username (leave blank to use default): ")
-        password = getpass("Enter the SSH password (no echo) (leave blank to use default): ")
-        
-        # Set the username and password to defaults if they are left blank
-        if username == "":
-            username = "root"
-        if password == "":
-            password = "emerson1"
-
-        # Ask for the gateway webpage username and password
-        if track_hart:
-            web_username = input("Enter the webpage username (leave blank to use default): ")
-            web_password = getpass("Enter the webpage password (no echo) (leave blank to use default): ")
-            
-            # Set the web username and password to defaults if they are left blank
-            if web_username == "":
-                web_username = "admin"
-            if web_password == "":
-                web_password = "default"
-
-        # Ask whether the user wants to use a time limit, a set number of measurements, or manually stop the test
-        print("1. Set test time limit")
-        print("2. Stop test manually")
-        use_time_limit = verify_int_input_option((1, 2), "Select 1 or 2: ") == 1
-
-        # Ask the user for the time limit if they want to use it
-        if use_time_limit:
-            time_limit = verify_int_input_range(lower_bound = 60, use_upper_bound = False, prompt = "Enter how long (in seconds) the test should run for: ")
-        
-        # Ask the user for the time between measurements
-        measurement_interval = verify_int_input_range(lower_bound = 10, use_upper_bound = False, prompt = "Enter how long (in seconds) between measurements: ")
-
-        # Ask the user if they want to track the memory usage of any processes
-        print("Enter a comma separated list (no spaces) of processes whose memory usage should be tracked")
-        processes_to_track = input("Leave the line empty to track no processes: ")
-
-    # Ask the user where results should be saved
-    if (options["UseSettingsFromConfigFile".lower()] == "no") or (options["UseAutomaticFilename".lower()] == "no"):
-        print("Choose a filename and location in the pop-up window")
-        root = tk.Tk()
-        filename = fd.asksaveasfilename(confirmoverwrite = True, defaultextension = ".csv", title = "Enter a Name for the Data File", filetypes = (("CSV Files", ".csv"), ("All Files","*")))
-        root.destroy()
-    else:
-        filename = datetime.now().strftime(str(hostname) + " - %a %d %B %Y - %I-%M-%S %p Memory Usage.csv")
     
     folder = filename.split(".csv")[0]
     filename = folder + "/" + filename
@@ -510,16 +376,6 @@ def main():
     # Since the thread is a daemon, it will be automatically stopped once the user exits in the main thread
     recordingThread = threading.Thread(target = record_data, name = "DataRecording", args = (filename, gateway, scraper, measurement_interval, track_hart, track_isa), daemon = True)
     recordingThread.start()
-
-    print("Test Started")
-
-    # After starting the other thread, use the main thread to determine when the program quits
-    if use_time_limit:
-        time.sleep(time_limit)
-    else:
-        quit_input = ""
-        while quit_input != "quit":
-            quit_input = input("Type \"quit\" to stop data logging: ").lower()
     
     # Wait until manipulating_data is False to safely quit
     if manipulating_data:
@@ -532,12 +388,12 @@ def main():
     
     # Create a plot of the data after determine which devices to plot
     device_type_list = []
-    if track_hart:
-        device_type_list.append(1)
-    if track_isa:
-        device_type_list.append(2)
-    if track_hart and track_isa:
-        device_type_list.append(3)
+    # if track_hart:
+    #     device_type_list.append(1)
+    # if track_isa:
+    #     device_type_list.append(2)
+    # if track_hart and track_isa:
+    #     device_type_list.append(3)
 
     print("Generating plots...")
 
