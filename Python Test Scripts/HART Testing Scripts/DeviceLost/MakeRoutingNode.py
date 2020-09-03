77415ca54a20bea1ac5b06843bae3b92b634ccf9
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-09-03 13:34:02 -0500

    Routing Node is Now a Single Point of Failure
    Devices could not join the network in a reasonable amount of time if they were forced to connect to only a single parent node. Advertisements from other devices would cause transmitters to attempt to join and fail for significant amounts of time.
    
    Now, every device except for the specified parent node cannot connect to the gateway. Instead, the parent node connects to the gateway, and then only one other node needs to connect to the parent node. Then every other device can connect with either the parent or any other connected transmitted as long as it is not the gateway.

--- Python Test Scripts/HART Testing Scripts/DeviceLost/MakeRoutingNode.py
+++ Python Test Scripts/HART Testing Scripts/DeviceLost/MakeRoutingNode.py
@@ -9,7 +9,6 @@ from InteractiveSSH import InteractiveSSH
 
 hostname = "toc0"
 parent_mac = "00-1B-1E-26-5D-6B-0E-BC"
-number_of_child_nodes = 3
 
 # Create the SSH session and open nwconsole
 ssh = InteractiveSSH(hostname)
@@ -18,30 +17,25 @@ ssh.start_nwconsole()
 # Enable ppath command
 ssh.safe_send("su becareful")
 mac_id_pairs = ssh.get_mote_id_mac_associations(use_mac_keys = True, include_ap = False)
+mac_id_pairs_with_ap = ssh.get_mote_id_mac_associations(use_mac_keys = True, include_ap = True)
 
-# Get the keys which correspond to MAC addresses
-macs = []
-for mac in mac_id_pairs:
-    macs.append(mac)
-
-# Select N MAC addresses as random
-child_macs = []
-while len(child_macs) < number_of_child_nodes:
-    mac = random.choice(macs)
-    if mac != parent_mac:
-        if mac not in child_macs:
-            child_macs.append(mac)
+# Determine which MAC is the AP
+ap_mac = ""
+for mac in mac_id_pairs_with_ap:
+    if mac not in mac_id_pairs:
+        ap_mac = mac
 
 # Clear any paths already present
 ssh.safe_send("ppath clear")
 
-# Create the paths using the ppath command
-for mac in child_macs:
-    print(ssh.safe_send("ppath only #" + mac + " #" + parent_mac))
-    print("Created path from " + mac + " to " + parent_mac + " (Mote " + mac_id_pairs[mac] + "->Mote " + mac_id_pairs[parent_mac] + ")")
+# Make all motes except the DUT unable to connect to the AP
+for mac in mac_id_pairs:
+    if (mac != parent_mac) and (mac != ap_mac):
+        print(ssh.safe_send("ppath never #" + mac + " #" + ap_mac))
 
 # Reset the gateway
 ssh.safe_send("exec reset mote 1")
 
+# End the SSH session
 ssh.shell.close()
 ssh.close()
