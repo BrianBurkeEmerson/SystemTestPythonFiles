f231e5c8b582a5e64ef98dde7392b6c98b118da7
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-09-15 14:24:06 -0500

    Added ISA100 Capability to ManagerReset.py
    Note that to run tests on gateways without ISA, the call to testObj.start() will need to have the second parameter changed to False.

--- Python Test Scripts/HART Testing Scripts/ManagerReset/ManagerReset.py
+++ Python Test Scripts/HART Testing Scripts/ManagerReset/ManagerReset.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import threading
 import tkinter as tk
 import tkinter.filedialog as fd
@@ -10,11 +11,14 @@ from StateTimeTracker import StateTimeTracker
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../SSHHelper")
 from InteractiveSSH import InteractiveSSH
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../ISA 100 Testing Scripts/ISADeviceCount")
+from ISADeviceCount import IsaDeviceCounter
 
-HOSTNAME = "toc0"
+
+HOSTNAME = "1410s-charlie"
 
 
-def write_to_numbered_file(folder, subfolder, data):
+def write_to_numbered_file(folder, subfolder, data, extension = ".txt"):
     if not os.path.exists(folder + "/" + subfolder):
         os.mkdir(folder + "/" + subfolder)
     
@@ -24,10 +28,10 @@ def write_to_numbered_file(folder, subfolder, data):
     while True:
         next_num += 1
 
-        if (str(next_num).zfill(4) + ".txt") not in files:
+        if (str(next_num).zfill(4) + extension) not in files:
             break
     
-    with open(folder + "/" + subfolder + "/" + str(next_num).zfill(4) + ".txt", "w") as f:
+    with open(folder + "/" + subfolder + "/" + str(next_num).zfill(4) + extension, "w") as f:
         f.write(data)
 
 
@@ -35,11 +39,17 @@ def main():
     global HOSTNAME
 
     directory = os.getcwd().replace("\\", "/")
-    print("Results will be stored in " + directory + " unless changed")
+    print("\033[93mResults will be stored in " + directory + " unless changed\033[0m")
     
-    asyncSsh = InteractiveSSH(HOSTNAME)
-    asyncSsh.shell.close()
-    asyncSsh.close()
+    while True:
+        try:
+            asyncSsh = InteractiveSSH(HOSTNAME)
+            asyncSsh.shell.close()
+            asyncSsh.close()
+            break
+        except:
+            print("SSH connection to " + str(HOSTNAME) + " not available. Retrying...")
+            time.sleep(1)
 
     testObj = StateTimeTracker(HOSTNAME)
     testThread = None
@@ -47,7 +57,7 @@ def main():
     # Enter a loop for executing commands
     cmd = "help"
     while cmd != "quit":
-        if (cmd == "short") or (cmd == "life") or (cmd == "cur"):
+        if (cmd == "short") or (cmd == "life") or (cmd == "cur") or (cmd == "isa"):
             try:
                 asyncSsh.safe_send("\n")
             except OSError:
@@ -59,6 +69,7 @@ def main():
             print("short: Record \"show stat 0\" and \"show stat 1\"")
             print("life: Record \"show stat life\"")
             print("cur: Record \"show stat cur\"")
+            print("isa: Record ISA100 path RSSI statistics")
             print("folder: Select folder where results are stored")
             print("start: Start test if one is not running")
             print("stop: Stop running test if one is active")
@@ -75,18 +86,38 @@ def main():
         elif cmd == "cur":
             data = asyncSsh.show_stat_cur(combine_list_to_string = True)
             write_to_numbered_file(directory, "show_stat_cur", data)
+        
+        elif cmd == "isa":
+            dbDownloader = IsaDeviceCounter(HOSTNAME)
+            dbDownloader.download_db_file()
+            dbDownloader.close()
+            rssis = dbDownloader.get_path_rssi()
+            id_name = dbDownloader.get_device_id_name_pairs()
+
+            # Compile the RSSIs into a string to be written into a CSV file
+            csv_content = "DeviceA,DeviceB,NameA,NameB,RSSI\n"
+            for path in rssis:
+                moteA = str(path["MoteA"])
+                moteB = str(path["MoteB"])
+                nameA = str(id_name[int(moteA)])
+                nameB = str(id_name[int(moteB)])
+                csv_content += (moteA + "," + moteB + "," + nameA + "," + nameB + "," + str(path["ABPower"]) + "\n")
+            
+            # Write the data into a file
+            write_to_numbered_file(directory, "isa_rssis", csv_content, ".csv")
 
         elif cmd == "folder":
             root = tk.Tk()
             directory = fd.askdirectory()
             root.destroy()
+            print("\033[93mTest results will be stored in " + directory + "\033[0m")
         
         elif cmd == "start":
             if testObj.running_test:
-                print("ERROR: Test already running. Stop current test before starting new one.")
+                print("\033[91mERROR: Test already running. Stop current test before starting new one.\033[0m")
             else:
                 print("Starting logging...")
-                testThread = threading.Thread(target = testObj.start, args = (directory,), name = "Active Test")
+                testThread = threading.Thread(target = testObj.start, args = (directory, True), name = "Active Test")
                 testThread.start()
         
         elif cmd == "stop":
@@ -95,7 +126,7 @@ def main():
                 testThread.join()
                 testThread = None
             else:
-                print("ERROR: No test currently running")
+                print("\033[91mERROR: No test currently running\033[0m")
         
         cmd = input("> ").lower()
     
@@ -103,6 +134,9 @@ def main():
     testObj.monitor_log = False
     if testThread != None:
         testThread.join()
+    
+    asyncSsh.shell.close()
+    asyncSsh.close()
 
 
 if __name__ == "__main__":
