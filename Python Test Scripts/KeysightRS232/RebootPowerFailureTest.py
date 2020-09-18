8eb2b04be49d2fc04e5475b8008a5b5286840b4e
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-09-18 11:47:39 -0500

    Power Cycle Test Writes Logs to Numbered Files

--- Python Test Scripts/KeysightRS232/RebootPowerFailureTest.py
+++ Python Test Scripts/KeysightRS232/RebootPowerFailureTest.py
@@ -16,12 +16,35 @@ GATEWAY_PORT = "/dev/ttyUSB0"
 continue_operation = True
 state = "Start"
 logging_start = datetime.now()
+directory = "power_cycle_test"
+
+
+def write_to_numbered_file(folder, extension = ".txt"):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    
+    files = os.listdir(folder)
+
+    next_num = -1
+    while True:
+        next_num += 1
+
+        if (str(next_num).zfill(6) + extension) not in files:
+            break
+    
+    filename = folder + "/" + str(next_num).zfill(6) + extension
+
+    with open(filename, "w") as f:
+        f.write("")
+    
+    return filename
 
 
 def power_cycle(port):
     global continue_operation
     global state
     global logging_start
+    global directory
 
     with E3647A(port) as s:
         while continue_operation:
@@ -41,9 +64,12 @@ def log_serial_output(port):
     global continue_operation
     global state
     global logging_start
+    global directory
 
     while continue_operation:
         if state == "Logging":
+            current_file = write_to_numbered_file(directory)
+
             with serial.Serial(port, 115200, timeout = 1) as s:
                 while state == "Logging":
                     resp = ""
@@ -54,15 +80,18 @@ def log_serial_output(port):
                             break
 
                     if resp != "":
-                        print(resp, end = "")
+                        with open(current_file, "w") as f:
+                            f.write(resp)
                     
                     if "login:" in resp:
                         state = "Shutdown"
-                        print("Successful boot")
+                        print("Successful boot. Logs written to " + current_file + ".")
 
                     if (datetime.now() - logging_start) > timedelta(minutes = 15):
                         state = "Shutdown"
-                        print("Failed boot")
+                        print("Failed boot. Logs written to " + current_file + ".")
+                        with open(directory + "/failed_boots.txt", "a") as f:
+                            f.write(current_file + "\n")
 
 
 with E3647A(POWER_SUPPLY_PORT) as s:
@@ -70,11 +99,16 @@ with E3647A(POWER_SUPPLY_PORT) as s:
     s.set_current(0)
     s.disable_output()
 
-#directory = input("Enter a folder name for log files: ")
-#os.mkdir(directory)
+directory = input("Enter a folder name for log files: ")
+os.mkdir(directory)
+
+with open(directory + "/failed_boots.txt", "w") as f:
+    f.write("")
 
 power_supply_thread = threading.Thread(target = power_cycle, args = (POWER_SUPPLY_PORT,), name = "Power Supply Cycling")
 serial_logging_thread = threading.Thread(target = log_serial_output, args = (GATEWAY_PORT,), name = "Serial Logging")
+
+time.sleep()
 
 power_supply_thread.start()
 serial_logging_thread.start()
