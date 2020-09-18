7f5822df922e23ca98da02ad8c3ebc9cc4de650b
Alex Stangl <67007903+alex-stangl-m@users.noreply.github.com>
2020-09-18 10:21:03 -0500

    Create E3647APowerSupply.py
    Library will automatically handle sending/returning commands via RS-232 to a Keysight E3647A power supply.

---
+++ Python Test Scripts/KeysightRS232/E3647APowerSupply.py
@@ -0,0 +1,48 @@
+# pip install pyserial
+import serial
+
+class E3647A(serial.Serial):
+    def __init__(self, port = "/dev/ttyS0", baud = 9600, timeout = 1):
+        self.port = port
+        self.baudrate = baud
+        self.timeout = timeout
+
+        self.open()
+
+    
+    def send_cmd(self, cmd = ""):
+        READ_AT_ONCE = 1000
+
+        self.write(bytes(cmd + "\n"))
+
+        return_string = ""
+        while True:
+            resp = self.read(READ_AT_ONCE).decode()
+            if resp == "":
+                break
+            else:
+                return_string += resp
+        
+        return return_string
+    
+
+    def set_voltage(self, voltage, max_current = False):
+        cmd = "APPL " + str(voltage)
+
+        if max_current:
+            cmd += ",MAX"
+        
+        self.send_cmd(cmd)
+
+
+    def set_current(self, current):
+        cmd = "SOUR:CURR " + str(current)
+        self.send_cmd(cmd)
+    
+
+    def enable_output(self):
+        self.send_cmd("OUTP:STAT ON")
+    
+
+    def disable_output(self):
+        self.send_cmd("OUTP:STAT OFF")
